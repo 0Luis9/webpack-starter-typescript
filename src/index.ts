e2b701686620350a6cc9b133b76afc1d14835a34
Luis <lsnglcrisostomo@gmail.com>
2021-03-31 14:54:21 -0400

    modifique un usuario de 2 formas

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ const usuario = {
     nombre: 'Maria',
     activo: true,
     fechaNaci: 0,
- }
+ } 
+
+ const usuariosRef = db.collection('usuarios');
+
 // Insert into usuarios...
 
 //db.collection('usuarios')
@@ -14,3 +17,13 @@ const usuario = {
 //    console.log( docRef )
 //})
 //.catch(e => console.log('error', e))
+
+
+//usuariosRef
+//.doc('mW7L4akxWWBLtoHa2VCz')
+//.set({
+//    activo: false,
+//   adad: 44,
+//    casado: true,
+//    id: 123
+//});
