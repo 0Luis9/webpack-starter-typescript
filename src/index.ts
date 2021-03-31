31a42207c9fa922bfde4312ddd6d0664d4bd691c
Luis <lsnglcrisostomo@gmail.com>
2021-03-31 14:45:35 -0400

    inserte un usuario con sus campos

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 
-import './Firebase/Config';
+import db from'./Firebase/Config';
 
+const usuario = {
+    nombre: 'Maria',
+    activo: true,
+    fechaNaci: 0,
+ }
+// Insert into usuarios...
+
+//db.collection('usuarios')
+//.add(usuario)
+//.then( docRef => {
+//    console.log( docRef )
+//})
+//.catch(e => console.log('error', e))
